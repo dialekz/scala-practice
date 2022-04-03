3f0e184c34d55a0ec1db5e673abefc6ec427577e
Dmitrii Kuznetsov <dialekz@gmail.com>
2022-04-03 23:58:57 +0300

    Refactoring for the init quill exercise

--- src/main/scala/module/quill_practice/Quill00CompileTimeQuotations.scala
+++ src/main/scala/module/quill_practice/Quill00CompileTimeQuotations.scala
@@ -8,9 +8,26 @@ class Quill00CompileTimeQuotations(ctx: JdbcContext[H2Dialect, SnakeCase.type]) 
 
   import ctx._
 
-  def filterByRadius(radius: Float): List[Circle] = run(q(radius))
+  // Quoting is implicit when writing a query in a run statement
+  def biggerThan(radius: Float): List[Circle] = run(biggerThanQ(radius))
 
-  private def q(radius: Float): Quoted[EntityQuery[Circle]] = quote {
-    query[Circle].filter(_.radius > lift(radius))
+  private def biggerThanQ(radius: Float): Quoted[EntityQuery[Circle]] = quote {
+    query[Circle].filter(_.radius > lift(radius)) // Need lift for parameter when using inside quote
+  }
+
+
+  // TODO literal-constants don't work:
+  //  // Run query inline without quote -> don't need lift for parameter
+  //  final val constantRadius: Float = 0.8f
+  //  final val queryWithConstant: EntityQuery[Circle] = query[Circle].filter(_.radius > constantRadius)
+  //  def biggerThanConstant: List[Circle] = run(queryWithConstant)
+
+
+  def findAllUsingLiftQuery(radiusList: List[Float]): List[Circle] = run {
+    query[Circle].filter(c => liftQuery(radiusList).contains(c.radius))
+  }
+
+  def insertBatch(circles: List[Circle]): List[Long] = run {
+    liftQuery(circles).foreach(c => query[Circle].insertValue(c))
   }
 }
--- src/test/scala/module/quill_practice/Quill00CompileTimeQuotationsTest.scala
+++ src/test/scala/module/quill_practice/Quill00CompileTimeQuotationsTest.scala
@@ -1,24 +1,55 @@
 package module.quill_practice
 
 import module.quill_practice.model.Circle
+import org.scalatest.BeforeAndAfterAll
 
-class Quill00CompileTimeQuotationsTest extends AbstractQuillTestWithContext {
+class Quill00CompileTimeQuotationsTest extends AbstractQuillTestWithContext with BeforeAndAfterAll {
 
   private val solution = for {
     ctx <- context
     solution = new Quill00CompileTimeQuotations(ctx)
   } yield solution
 
-  test("Test query filter by radius") {
-    val expected = Circle(radius = 0.5f)
+  private val expected = Circle(radius = 0.9f)
+  private val expected1 = Circle(radius = 0.1f)
+  private val expected2 = Circle(radius = 0.2f)
 
+  override def beforeAll(): Unit =
     for {
       s <- solution
-      _ = s.add(expected)
-      result = s.filterByRadius(0f)
-      empty = s.filterByRadius(1f)
+      _ = s.insertBatch(List(expected, expected1, expected2))
+    } yield ()
+
+  test("biggerThan should filter correct fresh inserted item") {
+    for {
+      s <- solution
+      result = s.biggerThan(0.8f)
+      empty = s.biggerThan(1f)
     } yield {
       assert(result.equals(List(expected)))
+      assert(empty.isEmpty)
+    }
+  }
+
+  // TODO literal-constants don't work
+  //  test("biggerThanConstant should filter correct fresh inserted item") {
+  //    for {
+  //      s <- solution
+  //      result = s.biggerThanConstant
+  //      empty = s.biggerThanConstant
+  //    } yield {
+  //      assert(result.equals(List(expected)))
+  //      assert(empty.isEmpty)
+  //    }
+  //  }
+
+  test("findAllUsingLiftQuery should filter correct fresh inserted items") {
+    for {
+      s <- solution
+      result = s.findAllUsingLiftQuery(List(0.1f, 0.2f))
+      empty = s.findAllUsingLiftQuery(List(1f))
+    } yield {
+      assert(result.equals(List(expected1, expected2)))
       assert(empty.isEmpty)
     }
   }
